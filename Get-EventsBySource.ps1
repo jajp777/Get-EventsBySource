6c7ef92b7a5cfcac510f8affa367463c452c7d1d
Wojciech Sciesinski <wojciech@sciesinski.net>
2015-10-20 00:55:45 +0200

    0.5.0 - 2015-10-19 - Code corrected based on PSScriptAnalyzer 1.1.0 output, support for more than logs (by name) added, help partially updated

--- Get-EventsBySource.ps1
+++ Get-EventsBySource.ps1
@@ -1,36 +1,72 @@
 ﻿Function Get-EventsBySource {
 <#
-	.SYNOPSIS
-	Function intended for remote gathering events data
+    .SYNOPSIS
+    Function intended to gather data from Windows events logs - the function Get-EventsBySource is wrapper for Get-WinEvent function
     
     .DESCRIPTION
-    Function intended to gather data from Windows events logs - Get-WinEvent is used
+    Function intended to gather data from Windows events logs - the function Get-EventsBySource is wrapper for Get-WinEvent function. Generally
+    the HashQuerySet parameter set is used but time span can be constructed based not only on start/end time but also 
+    
+    Function offer additional capabilities to merge multilines event description (using defined char as a lines separator)
+    and can limit amount of returned 
   
-	.PARAMETER ComputerName
-   
-	.PARAMETER LogName
-	
-	.PARAMETER ProviderName
-	
-	.PARAMETER EventID
+    .PARAMETER ComputerName
+    Gets events from the event logs on the specified computer. Type the NetBIOS name, an Internet Protocol (IP) address,
+    or the fully qualified domain name of the computer. The default value is the local computer.
+       
+    .PARAMETER LogName
+    Gets events from the specified event logs. Enter the event log names in a comma-separated list.
+    
+    .PARAMETER ProviderName
+    Gets events written by the specified event log providers. Enter the provider names in a comma-separated list, or use wildcard characters to create provider name patterns.
+    An event log provider is a program or service that writes events to the event log. It is not a Windows PowerShell provider.
+    Please remember that ProviderName is usually not equal with a source for event - please check an event XML to check used provider.
+    
+    .PARAMETER EventID
+    
     
     .PARAMETER StartTime
+    Date and time which will be used as the begining of a time period to query
     
     .PARAMETER EndTime
+    Date and time which will be used as the end of a time period to query
     
     .PARAMETER ForLastTimeSpan
+    Use number for which logs need to be queried - please select also correct "ForLastTimeUnit" value
     
     .PARAMETER ForLastTimeUnit
-	
-	.PARAMETER ConcatenateMessageLines
-	
-	.PARAMETER ConcatenatedLinesSeparator
-	
-	.PARAMETER MessageCharsAmount
-	 
+    Use the name of units for construct query.
+    
+    .PARAMETER ConcatenateMessageLines
+    For multilines events description lines will be merged by default. Please change to $false if you would not like this behaviour, than only first line can be handled.
+    
+    .PARAMETER ConcatenatedLinesSeparator
+    A char used to separated merged multilines event description. By default "^" is used due that is not usually used in events descriptions.
+    
+    .PARAMETER MessageCharsAmount
+    The number of chars which will be returned from event description.
+    
+    .INPUT
+    Cos
+    
+    .OUTPUT
+    Costam
+     
     .EXAMPLE
-    Get-EventsBySource
-         
+    Get-EventsBySource -ComputerName localhost -LogName application -ProviderName SecurityCenter -EventID 1,16 -ForLastTimeSpan 160 -ForLastTimeUnit minutes
+
+    ComputerName  : COMPUTERNAME.wojteks.lab
+    Source        : SecurityCenter
+    EventID       : 16
+    TimeGenerated : 10/19/2015 10:48:26 PM
+    Message       : The Windows Security Center Service could not stop Windows Defender
+
+    ComputerName  : COMPUTERNAME.wojteks.lab
+    Source        : SecurityCenter
+    EventID       : 1
+    TimeGenerated : 10/19/2015 10:48:23 PM
+    Message       : The Windows Security Center Service has started
+    
     .LINK
     https://github.com/it-praktyk/Get-EvenstBySource
     
@@ -39,14 +75,19 @@
           
     .NOTES
    
-    AUTHOR: Wojciech Sciesinski, wojciech.sciesinski@atos.net
+    AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
+            Parameters description partially based on Get-WinEvent help from PowerShell 3.0
+    
     KEYWORDS: Windows, Event logs, PowerShell
+    
     VERSION HISTORY
     0.3.1 - 2015-07-03 - Support for time span corrected, the first version published on GitHub
     0.3.2 - 2015-07-05 - Help updated, function corrected
     0.3.3 - 2015-08-25 - Help updated, to do updated
     0.4.0 - 2015-09-08 - Code reformated, Added support for more than one event id, minor update
-    
+    0.5.0 - 2015-10-19 - Code corrected based on PSScriptAnalyzer 1.1.0 output, support for more than logs (by name) added,
+                         help partially updated
+                         
 
     TODO
     - help update needed
@@ -80,19 +121,21 @@
 #>
     
     [CmdletBinding()]
+    [OutputType("System.Object[]")]
     param (
         [parameter(mandatory = $true)]
         [String]$ComputerName,
         [parameter(mandatory = $true)]
-        [String]$LogName,
+        [String[]]$LogName,
         [parameter(mandatory = $true)]
         [String]$ProviderName,
         [parameter(mandatory = $true)]
+        [alias("ID")]
         [Int[]]$EventID,
         [parameter(mandatory = $false, ParameterSetName = "StartEndTime")]
-        [DateTime]$StartTime,
+        [Nullable[DateTime]]$StartTime=$null,
         [parameter(mandatory = $false, ParameterSetName = "StartEndTime")]
-        [DateTime]$EndTime,
+        [Nullable[DateTime]]$EndTime=$null,
         [parameter(mandatory = $false, ParameterSetName = "ForLast")]
         [int]$ForLastTimeSpan = 24,
         [parameter(mandatory = $false, ParameterSetName = "ForLast")]
@@ -114,8 +157,6 @@
     }
     
     PROCESS {
-        
-        $SkipServer = $false
         
         Try {
             
@@ -197,7 +238,7 @@
                 
                 Write-Verbose -Message $MessageText
                 
-                $Events | ForEach  {
+                $Events | ForEach-Object -Process  {
                     
                     $Result = New-Object -TypeName PSObject
                     $Result | Add-Member -type NoteProperty -name ComputerName -value $_.MachineName
